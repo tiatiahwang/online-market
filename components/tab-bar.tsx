0e604f9f1fdde6d80f3669dd78186ae8e21d2d64
tiatiahwang <tiahwang44@gmail.com>
2024-09-02 13:09:08 -0700

    Etc: edit tab bar pathname

--- components/tab-bar.tsx
+++ components/tab-bar.tsx
@@ -10,28 +10,28 @@ export default function TabBar() {
   return (
     <div className='fixed bottom-0 w-full mx-auto max-w-sm grid grid-cols-4 border-t border-dark-bg dark:border-light-bg  px-5 py-3 *:text-light-text *:dark:text-dark-text'>
       <Link href='/home' className='flex flex-col items-center gap-px'>
-        {pathname === '/home' ? (
+        {pathname.includes('/home') ? (
           <HomeFullIcon width='30' height='30' fill='#60BC46' stroke='none' />
         ) : (
           <HomeIcon width='30' height='30' fill='none' stroke='#ECECEC' />
         )}
       </Link>
       <Link href='/life' className='flex flex-col items-center gap-px'>
-        {pathname === '/life' ? (
+        {pathname.includes('/life') ? (
           <LifeFullIcon width='30' height='30' fill='#60BC46' stroke='none' />
         ) : (
           <LifeIcon width='30' height='30' fill='none' stroke='#ECECEC' />
         )}
       </Link>
       <Link href='/chat' className='flex flex-col items-center gap-px'>
-        {pathname === '/chat' ? (
+        {pathname.includes('/chat') ? (
           <ChatFullIcon width='28' height='30' fill='#60BC46' stroke='none' />
         ) : (
           <ChatIcon width='28' height='30' fill='none' stroke='#ECECEC' />
         )}
       </Link>
       <Link href='/profile' className='flex flex-col items-center gap-px'>
-        {pathname === '/profile' ? (
+        {pathname.includes('/profile') ? (
           <UserFullIcon width='30' height='30' fill='#60BC46' stroke='none' />
         ) : (
           <UserIcon width='30' height='30' fill='none' stroke='#ECECEC' />
